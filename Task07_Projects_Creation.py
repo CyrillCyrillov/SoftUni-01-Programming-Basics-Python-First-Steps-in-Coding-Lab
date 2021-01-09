49f036d8c0f10217494ab4fd17db1f989d7626c7
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-09 19:29:44 +0200

    Add files via upload

---
+++ Task07_Projects_Creation.py
@@ -0,0 +1,5 @@
+name = input()
+number_of_projects = int(input())
+time_for_one_project = 3
+total_time = number_of_projects * time_for_one_project
+print(f"The architect {name} will need {total_time} hours to complete {number_of_projects} project/s.")

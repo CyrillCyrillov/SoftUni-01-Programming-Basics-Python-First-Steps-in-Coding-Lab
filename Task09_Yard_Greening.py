adbd54bf28e3e5a490ca21bea60d3884068463f5
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-09 19:30:17 +0200

    Add files via upload

---
+++ Task09_Yard_Greening.py
@@ -0,0 +1,6 @@
+area = float(input())
+price = 7.61
+discount = area * price * 0.18
+sum = (area * price) - discount
+print(f"The final price is: {sum} lv.")
+print(f"The discount is: {discount} lv.")
